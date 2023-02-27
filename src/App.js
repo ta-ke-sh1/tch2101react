1c56f6959548b6a93c8c012bba2132cf45dd14dc
Tiến Nam <nambeo23082001@gmail.com>
2023-02-27 13:22:41 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import React, { useState } from 'react'
 import StaffMain from "./screens/staff/main";
 import Login from "./screens/login";
 import AdminMain from "./screens/admin/main";
 import IdeaMain from "./screens/idea/ideaMain";
 import "./styles/main.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// import LoginToken from "./screens/containers/logintoken";
+
 
 const App = () => {
+    
     return useRoutes([
         {
             path: "/staff",
@@ -20,7 +26,7 @@ const App = () => {
             element: <IdeaMain />,
         },
         {
-            path: "/",
+            path: "/login",
             element: <Login />,
         },
     ]);
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,53 +1,97 @@
+import React, { useState } from "react";
 
+
+async function loginUser(credentials) {
+  return fetch("http://localhost:9000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => {data.json()
+console.log(data)});
+}
 export default function Login() {
-    return (
-        <>
-            <section class="vh-100">
-                <div class="container py-5 h-100">
-                    <div class="row d-flex align-items-center justify-content-center h-100">
-                        <div class="col-md-8 col-lg-7 col-xl-6">
-                             <input type="image" img src={`${process.env.PUBLIC_URL}/assets/images/login.png`} class="img-fluid" alt="Phone image" />
+    function setToken(token) {
+    localStorage.setItem('token',token);
+   
+  } 
+
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password,
+    });
+    setToken(token);
+  };
+
+  return (
+    <>
+      <section className="vh-100" style={{ backgroundColor: "#9A616D" }}>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-xl-10">
+              <div className="card" style={{ borderRadius: "1rem" }}>
+                <div className="row g-0">
+                  <div className="col-md-6 col-lg-5 d-none d-md-block">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/assets/images/login.png`}
+                      alt="login form"
+                      className="img-fluid"
+                      style={{ borderRadius: "1rem 0 0 1rem" }}
+                    />
+                  </div>
+                  <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div className="card-body p-4 p-lg-5 text-black">
+                      <form onSubmit={handleSubmit}>
+                        <div className="d-flex align-items-center mb-3 pb-1">
+                          <i
+                            className="fas fa-cubes fa-2x me-3"
+                            style={{ color: "#ff6219" }}
+                          ></i>
+                          <span className="h1 fw-bold mb-0">TCH2202</span>
                         </div>
-                        <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-                            <form>
-                                
-                                <div class="form-outline mb-4">
-                                    <input type="email" id="form1Example13" class="form-control form-control-lg" />
-                                    <label class="form-label" for="form1Example13">Email address</label>
-                                </div>
-
-                                <div class="form-outline mb-4">
-                                    <input type="password" id="form1Example23" class="form-control form-control-lg" />
-                                    <label class="form-label" for="form1Example23">Password</label>
-                                </div>
-
-                                <div class="d-flex justify-content-around align-items-center mb-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-                                        <label class="form-check-label" for="form1Example3"> Remember me </label>
-                                    </div>
-                                    <a href="#!">Forgot password?</a>
-                                </div>
-
-                                <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
-
-                                <div class="divider d-flex align-items-center my-4">
-                                    <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
-                                </div>
-
-                                <a class="btn btn-primary btn-lg btn-block" style={{ backgroundColor:'#3b5998'}} href="#!"
-                                    role="button" />
-                                    <i class="fab fa-facebook-f me-2"></i>Continue with Facebook
-                                </a>
-                                
-                                <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!"
-                                    role="button">
-                                    <i class="fab fa-twitter me-2"></i>Continue with Twitter</a>
-                            </form>
+                        <div className="form-outline mb-4">
+                          <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)}
+                            className="form-control form-control-lg"
+                          />
+                          <label className="form-label">Username</label>
                         </div>
+
+                        <div className="form-outline mb-4">
+                          <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="form-control form-control-lg"
+                          />
+                          <label className="form-label">Password</label>
+                        </div>
+
+                        <div className="pt-1 mb-4">
+                          <button
+                            className="btn btn-dark btn-lg btn-block"
+                            type="submit"
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </form>
                     </div>
+                  </div>
                 </div>
-            </section>
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
